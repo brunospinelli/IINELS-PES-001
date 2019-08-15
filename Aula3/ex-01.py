15d89ba55aa4bdb992d76d66410b51ea7b5a1ac6
Aluno <aluno@pc-10.isd.matriz>
2019-08-15 11:51:44 -0300

    Exemplo 01

--- Aula3/ex-01.py
+++ Aula3/ex-01.py
@@ -18,46 +18,46 @@ Muito acima do peso
 imc > 30.0
 
 '''
-# Calcula o IMC, dado um peso (Kg) e uma altura (m), e retorna o valor do IMC
+# Calcula o IMC, dado um peso (Kg) e uma altura (m), e retorna o valor do IMC.
 def calcula_imc(peso, altura):
   imc = (peso/(altura)**2)
   return(imc)
 
-# Retorna True ou False para a condição de: Muito abaixo do peso
+# Retorna True ou False para a condição de: Muito abaixo do peso.
 def muito_abaixo_do_peso(imc):
     print("Muito baixo do peso normal:")
     return(imc <= 17.0)
 
-# Retorna True ou False para a condição de: Abaixo do peso
+# Retorna True ou False para a condição de: Abaixo do peso.
 def abaixo_do_peso(imc):
     print("Abaixo do peso normal:")
     return(imc > 17.0 and imc <= 18.5)
 
-# Retorna True ou False para a condição de: Peso normal
+# Retorna True ou False para a condição de: Peso normal.
 def peso_normal(imc):
     print("Peso normal:")
     return(imc > 18.5 and imc <= 25.0)
 
-# Retorna True ou False para a condição de: Acima do peso
+# Retorna True ou False para a condição de: Acima do peso.
 def acima_do_peso(imc):
     print("Acima do peso normal:")
     return(imc > 25.0 and imc <= 30)
 
-# Retorna True ou False para a condição de: Muito acima do peso
+# Retorna True ou False para a condição de: Muito acima do peso.
 def muito_acima_do_peso(imc):
     print("Muito acima do peso normal:")
     return(imc > 30)
 
-# Recebe a altura e o peso
+# Recebe a altura e o peso.
 peso = float(input("Digite o peso (Kg):"))
 altura = float(input("Digite a altura (m):"))
 
-# Printa o IMC
+# Printa o IMC.
 imc = calcula_imc(peso, altura)
 print("Valor do IMC:")
 print(imc)
 
-# Printa as condições 
+# Printa as condições. 
 print(muito_abaixo_do_peso(imc))
 print(abaixo_do_peso(imc))
 print(peso_normal(imc))
